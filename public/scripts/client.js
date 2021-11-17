af50f585300c643bcff5f00e6ab04b588893b702
vagrant <arlo@gmail.com>
2021-11-17 20:29:29 +0000

    Added Hover over icons

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,18 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+/*  Hover Over Little Icons
+*/
+
+
+$(document).ready(function () {
+  // --- our code goes here ---
+    $("i").hover(function () {
+      
+      $(this).css("color", "yellow");
+    }, function(){
+    $(this).css("color", "black");
+  });
+
+});
+
